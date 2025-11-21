7c60b8b061496be2eba32a448ae0929a81ef2f84
Anurag srivastava <anuragsrivastava8874@gmail.com>
2025-11-21 23:52:21 +0530

    fix error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  //baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-
-  // useAuthStore.js
-baseURL: import.meta.env.MODE === "development" ? "http://localhost:3001" : "/api",
-
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3001/api"
+      : "/api",
   withCredentials: true,
 });
-
-
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: "http://localhost:3001/api", // matches your backend mount
-//   withCredentials: true,
-// });
