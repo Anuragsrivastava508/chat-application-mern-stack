d7b6cdcbd103de07a30586c3f379c0d959420174
Anurag srivastava <anuragsrivastava8874@gmail.com>
2025-11-26 15:36:51 +0530

    commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,21 @@
+// import jwt from "jsonwebtoken";
+
+// export const generateToken = (userId, res) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "7d",
+//   });
+
+//   res.cookie("jwt", token, {
+//     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+//     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+//     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+//     secure: process.env.NODE_ENV !== "development",
+//   });
+
+//   return token;
+// };
+
+
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
@@ -6,10 +24,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    httpOnly: true,
+    secure: true,        // required for HTTPS on Render
+    sameSite: "none",    // allow cross-domain cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
