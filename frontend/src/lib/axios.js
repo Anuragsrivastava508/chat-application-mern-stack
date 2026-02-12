9768eb434da722fad85cb82c96c505d0676477e8
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-02-13 00:38:15 +0530

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,4 @@
-// import axios from "axios";
 
-// const API_URL =
-//   import.meta.env.MODE === "production"
-//     ? import.meta.env.VITE_API_URL   // Production URL from .env.production
-//     : "http://localhost:5001/api";   // Local dev URL
-
-// export const axiosInstance = axios.create({
-//   baseURL: API_URL,
-//   withCredentials: true,
-// });
 import axios from "axios";
 
 const API_URL =
